8da8c604f65f5b0b85e3ed8e939eb3248327f379
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:07:13 -0500

    Refactor Customer class to remove Lombok and add methods
    Removed Lombok annotations and added explicit getters and setters for Customer class.

--- src/main/java/com/foodapp/model/Customer.java
+++ src/main/java/com/foodapp/model/Customer.java
@@ -8,35 +8,91 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class Customer {
-	
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer customerId;
-	private String fullName;
-	private Integer age;
-	private String gender;
-	private String mobileNumber;
-	private String email;
-	
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	private Address address;
-	
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JsonIgnore
-	private FoodCart foodCart;
-}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer customerId;
+    private String fullName;
+    private Integer age;
+    private String gender;
+    private String mobileNumber;
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private FoodCart foodCart;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public FoodCart getFoodCart() {
+        return foodCart;
+    }
+
+    public void setFoodCart(FoodCart foodCart) {
+        this.foodCart = foodCart;
+    }
+}
