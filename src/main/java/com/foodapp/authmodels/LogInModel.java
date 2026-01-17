2d2432f189c6207513e9ea6757656cf79bb024ee
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:13:38 -0500

    Update LogInModel.java

--- src/main/java/com/foodapp/authmodels/LogInModel.java
+++ src/main/java/com/foodapp/authmodels/LogInModel.java
@@ -4,22 +4,41 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class LogInModel {
-	
-	@Id
-	@NotNull
-	private Integer userId;
-	
-	private String userName;
-	private String password;
 
+    @Id
+    @NotNull
+    private Integer userId;
+
+    private String userName;
+    private String password;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
