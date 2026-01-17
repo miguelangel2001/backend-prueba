0a403b92c0e24693a000d3a2b7a485e98a89353b
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:05:09 -0500

    Update OrderDetails.java

--- src/main/java/com/foodapp/model/OrderDetails.java
+++ src/main/java/com/foodapp/model/OrderDetails.java
@@ -1,7 +1,6 @@
 package com.foodapp.model;
 
 import java.time.LocalDateTime;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,32 +9,65 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class OrderDetails {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer orderId;
-	private LocalDateTime orderDate;
-	private String orderStatus;
-	
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JsonIgnore
-	private FoodCart cart;
-	
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JsonIgnore
-	private  Bill bill;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer orderId;
+    private LocalDateTime orderDate;
+    private String orderStatus;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private FoodCart cart;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Bill bill;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public FoodCart getCart() {
+        return cart;
+    }
+
+    public void setCart(FoodCart cart) {
+        this.cart = cart;
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
 }
