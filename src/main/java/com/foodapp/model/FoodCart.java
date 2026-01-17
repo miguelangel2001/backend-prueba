5cfd57a6acf615233db62d9a8bd418e4005280aa
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:06:28 -0500

    Refactor FoodCart class to remove Lombok annotations
    Refactored FoodCart class by removing Lombok annotations and adding manual getter and setter methods.

--- src/main/java/com/foodapp/model/FoodCart.java
+++ src/main/java/com/foodapp/model/FoodCart.java
@@ -1,7 +1,6 @@
 package com.foodapp.model;
 
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,33 +10,46 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class FoodCart {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer cartId;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JsonIgnore
-	private Customer customer;
-	
-	
-	@OneToMany(targetEntity = Item.class,cascade = CascadeType.ALL)
-	private List<Item> itemList;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer cartId;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Customer customer;
 
+    @OneToMany(targetEntity = Item.class, cascade = CascadeType.ALL)
+    private List<Item> itemList;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
 }
