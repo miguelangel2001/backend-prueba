7c73ad98b4fae2357ca7b923dbbc04efe65ddf67
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:08:38 -0500

    Refactor Bill class to add explicit getters and setters

--- src/main/java/com/foodapp/model/Bill.java
+++ src/main/java/com/foodapp/model/Bill.java
@@ -1,7 +1,6 @@
 package com.foodapp.model;
 
 import java.time.LocalDateTime;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,28 +8,60 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class Bill {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer billId;
-	private LocalDateTime billDate;
-	private Double totalCost;
-	private Integer totalItem;
-	
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	private OrderDetails order;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer billId;
+    private LocalDateTime billDate;
+    private Double totalCost;
+    private Integer totalItem;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private OrderDetails order;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getBillId() {
+        return billId;
+    }
+
+    public void setBillId(Integer billId) {
+        this.billId = billId;
+    }
+
+    public LocalDateTime getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(LocalDateTime billDate) {
+        this.billDate = billDate;
+    }
+
+    public Double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(Double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public Integer getTotalItem() {
+        return totalItem;
+    }
+
+    public void setTotalItem(Integer totalItem) {
+        this.totalItem = totalItem;
+    }
+
+    public OrderDetails getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderDetails order) {
+        this.order = order;
+    }
 }
