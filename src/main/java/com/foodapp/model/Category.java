a5f95985884ff727138b72137fa16dcfea3c2a94
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:07:58 -0500

    Refactor Category class to remove Lombok usage
    Removed Lombok annotations and added manual getters and setters.

--- src/main/java/com/foodapp/model/Category.java
+++ src/main/java/com/foodapp/model/Category.java
@@ -1,25 +1,32 @@
 package com.foodapp.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Category {
-	
-	@Id
-	private Integer categoryId;
-	private String categoryName;
 
+    @Id
+    private Integer categoryId;
+    private String categoryName;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
