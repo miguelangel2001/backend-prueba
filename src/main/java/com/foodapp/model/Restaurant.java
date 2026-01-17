7af26966ffeac2327fda325fe4031de7e9481a4e
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:04:15 -0500

    Update Restaurant.java

--- src/main/java/com/foodapp/model/Restaurant.java
+++ src/main/java/com/foodapp/model/Restaurant.java
@@ -2,7 +2,6 @@ package com.foodapp.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,34 +9,74 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class Restaurant {
-	
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer restaurantId;
-	private String restaurantName;
-	private String managerName;
-	private String contactNumber;
-	
-	
-	@JsonIgnore
-	@OneToOne(cascade = CascadeType.ALL)
-	private Address address;
-	
-	@OneToMany(targetEntity = Item.class,cascade = CascadeType.ALL)
-	private List<Item> itemList = new ArrayList<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer restaurantId;
+    private String restaurantName;
+    private String managerName;
+    private String contactNumber;
+
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    @OneToMany(targetEntity = Item.class, cascade = CascadeType.ALL)
+    private List<Item> itemList = new ArrayList<>();
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Integer restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
 }
