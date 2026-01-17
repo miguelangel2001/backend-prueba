2c9cd5947009cba8c7bdd605cba2e1de2d61eac4
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:05:47 -0500

    Refactor Item class to use manual getters and setters
    Removed Lombok annotations and added manual getters and setters.

--- src/main/java/com/foodapp/model/Item.java
+++ src/main/java/com/foodapp/model/Item.java
@@ -7,27 +7,60 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
-public class Item{
-	
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer itemId;
-	private String itemName;
-	private Integer quantity;
-	private Double cost;
-	
-//	@Embedded
-	@OneToOne(cascade = CascadeType.ALL)
-	private Category category;
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer itemId;
+    private String itemName;
+    private Integer quantity;
+    private Double cost;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Category category;
+
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
