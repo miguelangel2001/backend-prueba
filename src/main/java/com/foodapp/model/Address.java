ca2c706b0c4fdb9718ece812aabe06efb93bab9c
miguelangel2001 <miguelangel01032001@gmail.com>
2026-01-17 14:09:23 -0500

    Refactor Address class to use manual getters and setters
    Removed Lombok annotations and added manual getters and setters for Address fields.

--- src/main/java/com/foodapp/model/Address.java
+++ src/main/java/com/foodapp/model/Address.java
@@ -5,26 +5,67 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class Address {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer addressId;
-	private String area;
-	private String city;
-	private String state;
-	private String country;
-	private String pincode;
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer addressId;
+    private String area;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
 
+    // ====================
+    // Getters y Setters
+    // ====================
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+}
